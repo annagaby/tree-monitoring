b5901d65a726ed6c1c674cf592d2bc2228a04691
anna gaby <annagaby@anna-macbook.local>
2019-09-12 12:13:36 -0700

    Moved all code to app

--- tree-monitoring/app.R
+++ tree-monitoring/app.R
@@ -1,7 +1,8 @@
 # Load relevant packages
+library(shiny)
+library(shinythemes)
 library(tidyverse)
 library(dplyr)
-library(shinythemes)
 library(leaflet)
 library(sf)
 library(lubridate)
@@ -13,7 +14,7 @@ ui <- fluidPage(
     theme = shinytheme("flatly"),
 
     # Application title
-    titlePanel("Reforestation Project - Sacha Waysa Community"),
+    titlePanel(tags$img(src = "https://images.squarespace-cdn.com/content/5be1911ecc8fed6b42ebe87e/1546689900224-9JJK8UZ1EEEXGBI7NS3P/logo+yakum_for+dark+BG.png?format=1500w&content-type=image%2Fpng", height = 80, "   Reforestation Project - Sacha Waysa Community")),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
@@ -23,15 +24,22 @@ ui <- fluidPage(
                         choices = c( "2019", "2020", "2021", "2022"),
                         selected = "2019"),
             tags$hr(style="border-color: gray;"),
-            p("Select year of reforestation project. ")   
+            p("Trees were planted in 2019 and monitored on a yearly basis until 2022. ")   
         ),
 
-        # Show a plot of the generated distribution, p and map
+        # Show 4 tabs for: reforestation map, species composition, growth, and mortalities
         mainPanel(
             tabsetPanel(type = "tabs",
                               tabPanel("Map", leafletOutput("mymap",height = 500)),
-                              tabPanel("Species", plotlyOutput("compositionPlot")),
-                              tabPanel("Mortalities", plotOutput("mortalityPlot"))
+                              tabPanel("Species", 
+                                       br(),
+                                       plotlyOutput("compositionPlot")),
+                              tabPanel("Growth",
+                                 plotOutput("fastestPlot"),
+                                 plotOutput("slowestPlot")),
+                              tabPanel("Mortalities",
+                                       br(),
+                                       plotOutput("mortalityPlot"))
             
             
            
@@ -48,26 +56,41 @@ ui <- fluidPage(
 server <- function(input, output) {
     
     # Read files
-    # tree_data <- read_csv("tree_monitoring.csv")
+    tree_data <- read_csv("tree_monitoring.csv")
     
     # Generate map ouput
     output$mymap <- renderLeaflet({
-        m
+        
+        # Filter by year and arrange data
+        tree_data_arranged = tree_data %>% 
+            filter( Year == input$year) %>% 
+            arrange(-Height) # arrange so that dead plants are last and red color shows over green
+        
+        # Create tree location map
+        leaflet(data = tree_data_arranged) %>%
+            addTiles() %>% 
+            addCircles(lng = ~Longitude,
+                       lat = ~Latitude,
+                       weight = 3, radius=2.5, 
+                       color= ~pal(Alive_or_Dead),
+                       stroke = TRUE, fillOpacity = 0.8,
+                       popup = paste("<strong>Tree ID</strong>:", tree_data_20$ID, "<br>",
+                                     "<strong>Species:</strong>", tree_data_20$Species, "<br>",
+                                     "<strong>Height:</strong>", tree_data_20$Height, " m" ))
+        
     })
     
     
     # Generate species composition output
     output$compositionPlot <- renderPlotly({
         
-        # Filtering tree data by year output
-        
+        # Filter tree data by year output
         tree_data_by_year <- tree_data %>% 
             filter(Year == input$year) %>% 
             group_by(Species) %>% 
             count(Species)
         
         # Create pie chart
-        
         plot_ly(tree_data_by_year, labels = ~Species, values = ~n, type = 'pie', textposition = 'inside',
                 textinfo = 'label+percent',
                 hoverinfo = 'text',
@@ -75,21 +98,44 @@ server <- function(input, output) {
                 showlegend = FALSE,
                 marker = list(colors = colors,
                               line = list(color = '#FFFFFF', width = 1))) %>%
-            layout(title = paste("Species Composition in Year", input$year),
+            layout(title = paste("Species Composition", input$year),
                    xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
                    yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))
         
     })
     
     
-    # Generate mortalities output
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    # Generate fastest growth output
+    output$fastestPlot <- renderPlot({
         
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    })
+    
+    # Generate slowest growth output
+    output$slowestPlot <- renderPlot({
+        
+    })
+    
+    
+    # Generate mortalities output
+    output$mortalityPlot <- renderPlot({
+        
+        # Filter mortalities by year
+        mortalities_by_year <- tree_data %>% 
+            select(Year, Cause_of_death) %>% 
+            filter( Cause_of_death != "NA") %>%
+            filter( Year == input$year) %>% 
+            count(Cause_of_death, Year) %>%
+            arrange(-n)
+        
+        # Create graph
+        ggplot(mortalities_by_year, aes( x = Cause_of_death, y = n)) +
+            geom_col() +
+            xlab("Cause of Death") +
+            ylab("Number of Dead Plants") +
+            theme_classic() +
+            ggtitle(paste("Causes of Plant Mortality", input$year))+
+            theme(plot.title = element_text(hjust = 0.5))
+        
     })
 }
 
